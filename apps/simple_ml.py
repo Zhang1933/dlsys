26d0dd67384861bc46726edf5ea124a423670298
Zhang1933 <193319076@qq.com>
2024-07-10 22:13:12 +0800

    fix parse_mnist function filepath empty bug

--- apps/simple_ml.py
+++ apps/simple_ml.py
@@ -33,33 +33,37 @@ def parse_mnist(image_filesname, label_filename):
                 for MNIST will contain the values 0-9.
     """
     ### BEGIN YOUR SOLUTION
-    with gzip.open(image_filesname, 'r') as f:
-        # first 4 bytes is a magic number
-        magic_number = int.from_bytes(f.read(4), 'big')
-        # second 4 bytes is the number of images
-        image_count = int.from_bytes(f.read(4), 'big')
-        # third 4 bytes is the row count
-        row_count = int.from_bytes(f.read(4), 'big')
-        # fourth 4 bytes is the column count
-        column_count = int.from_bytes(f.read(4), 'big')
-        # rest is the image pixel data, each pixel is stored as an unsigned byte
-        # pixel values are 0 to 255
-        image_data = f.read()
-        images = np.frombuffer(image_data, dtype=np.uint8)\
-            .reshape((image_count, row_count*column_count)).astype(np.float32)
-        min_val=np.min(images)
-        max_val=np.max(images)
-        images=(images - min_val) / (max_val - min_val)
+    images=None
+    if image_filesname:
+        with gzip.open(image_filesname, 'r') as f:
+            # first 4 bytes is a magic number
+            magic_number = int.from_bytes(f.read(4), 'big')
+            # second 4 bytes is the number of images
+            image_count = int.from_bytes(f.read(4), 'big')
+            # third 4 bytes is the row count
+            row_count = int.from_bytes(f.read(4), 'big')
+            # fourth 4 bytes is the column count
+            column_count = int.from_bytes(f.read(4), 'big')
+            # rest is the image pixel data, each pixel is stored as an unsigned byte
+            # pixel values are 0 to 255
+            image_data = f.read()
+            images = np.frombuffer(image_data, dtype=np.uint8)\
+                .reshape((image_count, row_count*column_count)).astype(np.float32)
+            min_val=np.min(images)
+            max_val=np.max(images)
+            images=(images - min_val) / (max_val - min_val)
     
-    with gzip.open('data/train-labels-idx1-ubyte.gz', 'r') as f:
-        # first 4 bytes is a magic number
-        magic_number = int.from_bytes(f.read(4), 'big')
-        # second 4 bytes is the number of labels
-        label_count = int.from_bytes(f.read(4), 'big')
-        # rest is the label data, each label is stored as unsigned byte
-        # label values are 0 to 9
-        label_data = f.read()
-        labels = np.frombuffer(label_data, dtype=np.uint8)
+    labels=None
+    if label_filename:
+        with gzip.open(label_filename, 'r') as f:
+            # first 4 bytes is a magic number
+            magic_number = int.from_bytes(f.read(4), 'big')
+            # second 4 bytes is the number of labels
+            label_count = int.from_bytes(f.read(4), 'big')
+            # rest is the label data, each label is stored as unsigned byte
+            # label values are 0 to 9
+            label_data = f.read()
+            labels = np.frombuffer(label_data, dtype=np.uint8)
     return images,labels
     ### END YOUR SOLUTION
 
